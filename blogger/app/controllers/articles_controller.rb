ae80491968bb0968f0df8428f9a97b2cb0ecc899
Eric Herman <hermaneric@outlook.com>
2016-10-12 12:52:46 -0400

    Finished part 11

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -17,12 +17,16 @@ class ArticlesController < ApplicationController
 	    @article = Article.new(article_params)
 	    @article.save
 
+	    flash.notice = "Article '#{@article.title}' Created!"
+
 	    redirect_to article_path(@article)
 	end
 
 	def destroy
 		@article = Article.find(params[:id]).destroy
 		redirect_to articles_path
+
+		flash.notice = "Article '#{@article.title}' Deleted!"
 
 	end
 
